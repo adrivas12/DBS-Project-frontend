01cfbc1be6b209f7ed6b0eb6938a529776f7559e
Aditi Shrivastava <88084673+adrivas12@users.noreply.github.com>
2023-05-11 15:58:52 +0530

    Add files via upload

---
+++ DBS_project/faqs.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" /> 
+  <title>FAQs</title>
+  <link href="css/style1.css" rel="stylesheet" />
+</head>
+
+<body class="faqs">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary ">
+    <div class="container-fluid">
+        <a href="index.html">
+        <img src="Images/GoodFood.png" alt=""/>
+        </a>
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+            <a class="nav-link" aria-current="page" href="owner_profile.html">Home</a>
+            <a class="nav-link1" href="FAQ.html">FAQs</a>
+        </ul>
+      </div>
+    </div>
+  </nav>
+  <div class="rect1"></div>
+  <div class="rect2"></div>
+  <div class="rect3"></div>
+  <div class="rect4"></div>
+  <div class="Questions">
+    Q: What is GoodFood?<br>
+    A:GoodFood is a platform where users can browse and leave reviews for different restaurants they have visited.<br>
+    <br>
+    Q: How do I use a GoodFood?<br>
+    A: You can use GoodFood by searching for restaurants in your area or by browsing through different categories such as cuisine type, price range, and rating.<br>
+    <br>
+    Q: Can anyone leave a review on GoodFood?<br>
+    A: Yes, anyone can leave a review on GoodFood as long as they have a registered account.<br>
+    <br>
+    Q: How reliable are the reviews on GoodFood?<br>
+    A: The reliability of the reviews on GoodFood can vary. It's important to read multiple reviews and take into account the overall rating of the restaurant.<br>
+    <br>
+    Q: Can I trust the ratings on GoodFood?<br>
+    A: The ratings on GoodFood can be a good indication of a restaurant's overall quality, but it's important to read reviews and consider the number of ratings before making a decision.<br>
+    <br>
+    Q: How can I write a helpful review on GoodFood?<br>
+    A: To write a helpful review on GoodFood, include details about your experience such as the food, service, and atmosphere. Be specific and honest about your opinions and avoid using profanity.<br>
+    <br>
+    Q: Are there any fees for using GoodFood?<br>
+    A: Most restaurant reviewing websites are free to use, but some may charge a fee for certain features such as making a reservation. It's best to check with the specific website for more information.<br>
+  </div>
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+  </script>
+
+</body>
+
+</html>
