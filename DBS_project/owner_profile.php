164c7250e9036bb20fa90984938b970cedc43727
Aditi Shrivastava <88084673+adrivas12@users.noreply.github.com>
2023-05-11 15:06:52 +0530

    Add files via upload

---
+++ DBS_project/owner_profile.php
@@ -0,0 +1,62 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+  <meta charset="utf-8" />
+  <meta http-equiv="X-UA-Compatible" content="IE=edge" />
+  <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+  
+
+  <title>Good Food</title>
+
+  <link href="css/style1.css" rel="stylesheet" />
+</head>
+
+<body class="owner_profile">
+  <nav class="navbar navbar-expand-lg bg-body-tertiary ">
+    <div class="container-fluid">
+        <a href="index.html">
+        <img src="Images/GoodFood.png" alt=""/>
+        </a>
+        <span class="navbar-toggler-icon"></span>
+      </button>
+      <div class="collapse navbar-collapse" id="navbarSupportedContent">
+        <ul class="navbar-nav mr-auto mb-2 mb-lg-0">
+            <a class="nav-link" aria-current="page" href="owner_profile.html">Home</a>
+            <a class="nav-link1" href="FAQ.html">FAQs</a>
+        </ul>
+      </div>
+    </div>
+  </nav>
+    <section class="home">
+      <div class="side_heading">
+        <div class="line"></div>
+      </div>
+    </section>
+    <section class="profile_area">
+        <div class="line2"></div>
+        <div class="line1"></div>
+        <div class="Lawrence">
+            LAWRENCE
+        </div>
+        <div class="owner_id">
+            <h5 class="oi">Owner ID: </h5>
+        </div>
+        <div class="userid">
+            <h5 class="ui">User ID: </h5>
+        </div>
+        <div class="line"></div>
+    </section>
+    <div class="add_restaurant_btn">
+        <a href="restaurant_addition.php">
+        <button type="button" class="rest_btn">Add Restaurant +</button>
+        </a>
+    </div>
+  <script type="text/javascript" src="js/jquery-3.4.1.min.js"></script>
+  <script type="text/javascript" src="js/bootstrap.js"></script>
+  <script type="text/javascript" src="https://cdnjs.cloudflare.com/ajax/libs/OwlCarousel2/2.3.4/owl.carousel.min.js">
+  </script>
+
+</body>
+
+</html>
